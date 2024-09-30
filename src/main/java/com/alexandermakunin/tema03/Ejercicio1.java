3ef76f31872551e30599d64d52cabf747093619e
Alexander Makunin <amakunin7@gmail.com>
2024-09-30 20:41:16 +0200

    Modificar Ejercicio1

--- src/main/java/com/alexandermakunin/tema03/Ejercicio1.java
+++ src/main/java/com/alexandermakunin/tema03/Ejercicio1.java
@@ -3,5 +3,6 @@ package com.alexandermakunin.tema03;
 public class Ejercicio1 {
     public static void main(String[]args) {
         System.out.print("Ejercicio 1");
+        System.out.print("He editado este archivo");
     }
 }
