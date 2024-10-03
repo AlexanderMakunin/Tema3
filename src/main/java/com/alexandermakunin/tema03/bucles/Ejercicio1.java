8da23f8e3437fa5f504b0c807e72a0fff7c7eb6e
Alexander Makunin <amakunin7@gmail.com>
2024-10-03 20:20:35 +0200

    hacer los primeros ejercicios

---
+++ src/main/java/com/alexandermakunin/tema03/bucles/Ejercicio1.java
@@ -0,0 +1,9 @@
+package com.alexandermakunin.tema03.bucles;
+
+public class Ejercicio1 {
+    public static void main(String [] args) {
+        for ( int x = 45; x <101; x++) {
+            System.out.println(x);
+        }
+    }
+}
---
+++ src/main/java/com/alexandermakunin/tema03/bucles/Ejercicio2.java
@@ -0,0 +1,10 @@
+package com.alexandermakunin.tema03.bucles;
+
+public class Ejercicio2 {
+    public static void main(String [] args) {
+        int suma = 0;
+        for ( int x = 1; x <1001; x++) {
+            suma = suma + x;
+        } System.out.println("La suma es " + suma);
+    }
+}
