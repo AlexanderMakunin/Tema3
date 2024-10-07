154b61206c922f332b2bf1810c2893bb9ef075c2
Alexander Makunin <amakunin7@gmail.com>
2024-10-07 21:02:47 +0200

    Empezar tarea 7

---
+++ src/main/java/com/alexandermakunin/tema03/bucles/Ejercicio6.java
@@ -0,0 +1,20 @@
+package com.alexandermakunin.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String [] args) {
+        Scanner leer = new Scanner(System.in);
+        int a;
+        int b = 0;
+        do {
+            System.out.println("Indique un numero");
+            a = Integer.parseInt(leer.nextLine());
+        }
+            while(a < 0 && a > 10 );
+        for ( int x = 1; x <= 10; x++) {
+            b = a + b;
+            System.out.println("El resultado es: "+b);
+        } leer.close();
+    }
+}
---
+++ src/main/java/com/alexandermakunin/tema03/bucles/Ejercicio7.java
@@ -0,0 +1,16 @@
+package com.alexandermakunin.tema03.bucles;
+
+public class Ejercicio7 {
+    public static void main(String [] args) {
+        int a = 4;
+        int b = 5;
+        int c = 0;
+        do {
+        }
+            while(a < 0 && a > 10 );
+        for ( int x = 1; x <= b; x++) {
+            c = a * a;
+            System.out.println("El resultado es: "+c);
+        }
+    }
+}
